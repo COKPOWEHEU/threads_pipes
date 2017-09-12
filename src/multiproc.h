a32a2e161368b8fec2cf2d01a4e97b1ce7e8f07a
COKPOWEHEU <31559186+COKPOWEHEU@users.noreply.github.com>
2017-09-12 15:08:14 +0400

    change WinAPI-pipes to homemade (see multiproc.c)
    I don't know how to use WinAPI pipes. This implementation at least works. May be it is slower, but it can be more portable.
    Programmed pipes are ring buffers in shared memory.

--- src/multiproc.h
+++ src/multiproc.h
@@ -1,5 +1,6 @@
 #ifndef _MULTIPROC_H_
 #define _MULTIPROC_H_
+#include "portability.h"
 
 //совместимость 
 #ifdef __cplusplus
@@ -9,40 +10,54 @@ extern "C" {
 #define MTP_OK  0
 #define MTP_ERR 1
 
-#if (defined(linux))
+#if defined(linux)
   #include <pthread.h>
-  typedef union {int fd;}mppipe_w; //дескриптор канала на запись
-  typedef union {int fd;}mppipe_r; //дескриптор канала на чтение
-  typedef union {pthread_t thread;}mpthread_t; //дескриптор потока
-  typedef union {void *attr;}mpthread_arrt_t; //атрибуты при создании потока (не используется)
-#elif (defined(WIN32))
+  typedef union {pthread_t thread;}mpthread_t; //дескриптор потока //thread descriptor
+#elif defined(WIN32)
   #include <windows.h>
-  typedef union {HANDLE fd;}mppipe_w; //FIFO write descriptor
-  typedef union {HANDLE fd;}mppipe_r; //FIFO read descriptor
-  typedef union {HANDLE thread;}mpthread_t; //thread descriptor
-  typedef union {void *attr;}mpthread_arrt_t; //thread attributes (not used yet)
+  //можно было использовать POSIX реализацию, но она тянет за собой libwinpthread-1.dll
+  //зачем оно когда и родные Win32-потоки работают нормально
+  typedef union {HANDLE thread;}mpthread_t;
+  #define MPPIPE_BYPASS
 #else
   #error architecture does not supported yet
 #endif
+  typedef union {void *attr;}mpthread_arrt_t; //атрибуты при создании потока (не используется) //thread attributes (ignore yet)
   
-//получить количество ядер/процессоров (если не удалось - 1, т.к. хоть один процессор точно есть)
+#ifdef MPPIPE_BYPASS
+/*читать - КОСТЫЛЬ. Ручное создание FIFO буфера и работа с ним
+  это менее отлаженный вариант. Наверняка более медленный, зато, наверное, более переносимый
+  Но в целом - если есть нормальные каналы (не тот ужас, что считается каналами в WinAPI) лучше использовать их
+*/
+  typedef union {void *fifo;}mppipe_w; //дескриптор канала на запись
+  typedef union {void *fifo;}mppipe_r; //дескриптор канала на чтение
+#elif defined(linux)
+  typedef union {int fd;}mppipe_w; //write descriptor of pipe
+  typedef union {int fd;}mppipe_r; //read descriptor of pipe
+#else
+  #error normal pipes not supported for this architecture. Try to define MPPIPE_BYPASS
+#endif
+  
+//получить количество ядер/процессоров
+//return number of processors
 unsigned int system_cpu_number();
 
 //создание нового потока. Результат 0 - успех, остальное - ошибки
+//create new thread. Return value: 0 - success, other - errors
 int mpthread_create(mpthread_t *res, void *(*func)(void *), void *param, mpthread_arrt_t *attr);
 //ожидание завершения потока. Результат 0 - успех, остальное - ошибки
+//wait for finishing of thread. Return value: 0 - success, other - errors
 int mpthread_join(mpthread_t thread);
   
 //создание анонимного канала. Результат MTP_ERR - ошибка, MTP_OK - успех
-//create anonymous pipe. Return value: MTP_ERR - error, MTP_OM - success
+//create anonymous pipe. Return value: MTP_OK - success, MTP_ERR - error
 char mppipe_create_anon(mppipe_w *wr, mppipe_r *rd);
-
 //чтение и запись в канал. buf-откуда читать/писать, count-сколько байт. Результат - сколько реально прочитано/записано
 //если операция невозможна - функция не блокируется, возвращаемое значение 0
-//read and write into pipe. buf - data to read/write, count - number of bytes to read/write. Return value - number of bytes really read/written 
+//read and write into pipe. rd/wd - pipe descriptor, buf - data to read/write, count - number of bytes
+//return value - number ob bytes actually read/write. If pipe empty/full, function does not block
 unsigned long mppipe_read(mppipe_r rd, void *buf, size_t count);
 unsigned long mppipe_write(mppipe_w wr, void *buf, size_t count);
-
 //закрытие канала
 //close pipe
 char mppipe_close_write(mppipe_w *wr);
