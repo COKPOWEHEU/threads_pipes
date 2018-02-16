4d5ae0ceb4783e783ec39e446442c0f1372e1590
COKPOWEHEU <31559186+COKPOWEHEU@users.noreply.github.com>
2018-02-16 12:30:51 +0300

    Update portability.h
    add pusleep() function because mingw's one sometimes does not work

--- src/portability.h
+++ src/portability.h
@@ -1,6 +1,7 @@
 /*
  *  Linux
  */
+#include <inttypes.h>
 #if defined(linux) || defined(__linux) || defined(__linux__) || defined(__GNU__) || defined(__GLIBC__)
   #ifndef linux
     #define linux 1 //чтобы была всего одна константа, а не десяток
@@ -10,6 +11,7 @@
   #define DynLoad(s) dlopen(s, RTLD_LAZY)
   #define DynFunc(lib, name)  dlsym(lib,name)
   #define DynClose(lib) dlclose(lib)
+  #define pusleep(usec) usleep(usec)
 /*
  *  Win 32
  */
@@ -24,6 +26,15 @@
   #define DynLoad(s) LoadLibrary(s)
   #define DynFunc(lib, name)   GetProcAddress((HINSTANCE)lib, name)
   #define DynClose(lib) FreeLibrary(lib)
+  static inline void pusleep(uint64_t usec){
+    HANDLE timer; 
+    LARGE_INTEGER ft; 
+    ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+    timer = CreateWaitableTimer(NULL, TRUE, NULL); 
+    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0); 
+    WaitForSingleObject(timer, INFINITE); 
+    CloseHandle(timer); 
+  }
  /*
   *  Other systems (unsupported)
   */
